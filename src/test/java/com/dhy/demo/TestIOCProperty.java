76c98d355c4c56974880547736eb04d045474d7c
丁浩阳 <3003763007@qq.com>
2019-12-02 21:40:39 +0800

    add

--- src/test/java/com/dhy/demo/TestIOCProperty.java
+++ src/test/java/com/dhy/demo/TestIOCProperty.java
@@ -5,6 +5,9 @@ import com.dhy.demo.entity.Student;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.io.UnsupportedEncodingException;
 
 /**
  * @author dinghy
@@ -13,8 +16,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class TestIOCProperty {
     ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
     @Test
-    public void test(){
+    public void test() throws UnsupportedEncodingException {
         Student student = (Student) applicationContext.getBean("student");
         System.out.println(student);
+        Environment environment = applicationContext.getEnvironment();
+        String age = environment.getProperty("age");
+        System.out.println(age);
+        String nickName=environment.getProperty("nickName");
+        System.out.println(new String (nickName.getBytes(),"UTF-8"));
     }
 }
